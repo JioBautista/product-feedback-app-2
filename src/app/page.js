330d89f96c07cac6dac8d315d414b326ca5bf414
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-12-13 18:51:42 -0500

    Updates

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,15 @@
-export default function Home() {
+import { sql } from "@vercel/postgres";
+
+export default async function Home() {
+  const { rows } = await sql`SELECT * FROM productrequests `;
+
+  // const currentUser = rows.filter((user) => user.username === "jpacho1234");
+
+  // console.log(currentUser);
   return (
     <div>
       <h1>Hello World</h1>
+      <h2>This is the current user</h2>
     </div>
   );
 }
