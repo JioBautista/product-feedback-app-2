6fbd5d7df6496aa8eedb405233697b746d80eca6
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2025-01-18 12:07:27 -0500

    Update page.js

--- src/app/roadmap/page.js
+++ src/app/roadmap/page.js
@@ -3,10 +3,10 @@ import Button from "../components/Button";
 
 export default function Roadmap() {
   return (
-    <div className="border  border-black">
-      <Container>
+    <Container>
+      <div className="border  border-black">
         <h1>Roadmap</h1>
-      </Container>
-    </div>
+      </div>
+    </Container>
   );
 }
