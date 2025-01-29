3a2b16cb5436ef0bd901ee23cc72f5df00382098
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2025-01-29 13:07:20 -0500

    Updates
    Sorting and filtering data completed

--- src/app/components/Filters.jsx
+++ src/app/components/Filters.jsx
@@ -1,18 +1,31 @@
+"use client";
 import Link from "next/link";
+import { useSearchParams, usePathname } from "next/navigation";
+import { useCallback } from "react";
 
 export default function Filters() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
   const buttons = [
     { name: "All", link: "/" },
-    { name: "UI", link: "/?filter=ux" },
-    { name: "UX", link: "/?filter=ux" },
-    { name: "Enhancement", link: "/?filter=enhancement" },
-    { name: "Bug", link: "/?filter=bug" },
+    { name: "UI", link: "UI" },
+    { name: "UX", link: "UX" },
+    { name: "Enhancement", link: "Enhancement" },
+    { name: "Bug", link: "Bug" },
     {
       name: "Feature",
-      link: `/?filter=feature`,
+      link: `Feature`,
     },
   ];
 
+  const createQueryString = useCallback(
+    (name, value) => {
+      const params = new URLSearchParams(searchParams);
+      params.set(name, value);
+      return params.toString();
+    },
+    [searchParams]
+  );
   return (
     <div className="bg-white rounded-lg flex items-center flex-wrap gap-2 p-5">
       {buttons.map((elements, index) => (
@@ -20,7 +33,11 @@ export default function Filters() {
           className="px-4 py-2 bg-[#F2F4FF] text-[#4661E6] font-bold rounded-2xl"
           key={index}
         >
-          <Link href={elements.link}>{elements.name}</Link>
+          <Link
+            href={pathname + "?" + createQueryString("filter", elements.link)}
+          >
+            {elements.name}
+          </Link>
         </button>
       ))}
     </div>
--- src/app/lib/fetchProducts.js
+++ src/app/lib/fetchProducts.js
@@ -1,19 +1,12 @@
 import { sql } from "@vercel/postgres";
-export default async function fetchProducts(filter, sort) {
+
+export default async function fetchProducts(sort) {
   const { rows } = await sql`SELECT * FROM products`;
-  if (filter) {
-    const { rows } =
-      await sql`SELECT * FROM products WHERE category = ${filter}`;
-    return rows;
-  } else if (sort === "Most Upvotes") {
+  if (sort === "Most Upvotes") {
     const { rows } = await sql`SELECT * FROM products ORDER BY upvotes DESC`;
     return rows;
   } else if (sort === "Least Upvotes") {
     const { rows } = await sql`SELECT * FROM products ORDER BY upvotes ASC`;
-    return rows;
-  } else if ((filter, sort === "Most Upvotes")) {
-    const { rows } =
-      await sql`SELECT * FROM products WHERE category = ${filter} ORDER BY upvotes ASC`;
     return rows;
   }
   return rows;
