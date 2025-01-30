1ed00622f64a034f37b0317e1cb7ac47a2bfe997
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2025-01-30 15:19:39 -0500

    Updates

--- src/app/components/Product.jsx
+++ src/app/components/Product.jsx
@@ -1,16 +1,18 @@
 import Link from "next/link";
 
-export default function Product({ data }) {
+export default function Product({ data, border }) {
   const spanStyles =
     "col-span-2 justify-self-start px-4 py-2 bg-[#F2F4FF] text-[#4661E6] font-bold rounded-full";
   return (
-    <Link href={`/${data.productid}`} className="block">
-      <div className="bg-white p-5 rounded-md grid grid-cols-2 gap-2">
-        <p className="font-bold col-span-2">{data.title}</p>
-        <p className="text-gray-500 col-span-2">{data.description}</p>
-        <span className={spanStyles}>{data.category}</span>
-        <span className={spanStyles}>{data.upvotes}</span>
-      </div>
-    </Link>
+    <div className={border ? `border-t-8 rounded-md ${border}` : null}>
+      <Link href={`/${data.productid}`} className="block">
+        <div className="bg-white p-5 rounded-md grid grid-cols-2 gap-2">
+          <p className="font-bold col-span-2">{data.title}</p>
+          <p className="text-gray-500 col-span-2">{data.description}</p>
+          <span className={spanStyles}>{data.category}</span>
+          <span className={spanStyles}>{data.upvotes}</span>
+        </div>
+      </Link>
+    </div>
   );
 }
--- src/app/components/Tabs.jsx
+++ src/app/components/Tabs.jsx
@@ -6,25 +6,30 @@ export default async function Tabs() {
   const data = await fetchProducts();
   const plannedData = data ? data.filter((x) => x.status === "Planned") : null;
   const inProgressData = data
-    ? data.filter((x) => x.status === "In-Progress")
+    ? data.filter((x) => x.status === "In-progress")
     : null;
 
   const liveData = data ? data.filter((x) => x.status === "Live") : null;
-
-  console.log(data);
   return (
     <div className="flex items-center">
       <Tab
         href={"/roadmap/planned"}
         text={"Planned"}
         data={plannedData.length}
+        borderT={"border-b-[#F49F85] border-b-4"}
       />
       <Tab
         href={"/roadmap/in-progress"}
         text={"In-Progress"}
         data={inProgressData.length}
+        borderT={"border-b-[#AD1FEA] border-b-4"}
       />
-      <Tab href={"/roadmap/live"} text={"Live"} data={liveData.length} />
+      <Tab
+        href={"/roadmap/live"}
+        text={"Live"}
+        data={liveData.length}
+        borderT={"border-b-[#62BCFA] border-b-4"}
+      />
     </div>
   );
 }
