71fe73a6297f98ab7bd6c2ccacecaf447bb1f7d1
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2025-01-31 12:02:55 -0500

    Update Tab.jsx

--- src/app/components/Tab.jsx
+++ src/app/components/Tab.jsx
@@ -5,14 +5,13 @@ import Link from "next/link";
 export default function Tab({ href, data, text, borderT }) {
   const [border, setBorder] = React.useState("border-b-gray border-b-2");
   return (
-    <div
-      className={`py-5 px-1 ${border} w-full text-center text-gray-500 font-bold`}
+    <Link
+      href={href}
+      className={`block p-5 ${border} w-full text-center text-gray-500 font-bold`}
       onClick={() => setBorder(borderT)}
       onBlur={() => setBorder("border-b-gray border-b-2")}
     >
-      <Link href={href}>
-        {text} ({data})
-      </Link>
-    </div>
+      {text} ({data})
+    </Link>
   );
 }
