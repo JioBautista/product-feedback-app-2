1ea3f0ebeb6dd71e51d3791f57ab9806e3829881
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2025-02-03 14:50:38 -0500

    Updates
    Fixed the upvotes button component. Will continue on media query for tablet/desktop for the Product component later.

--- src/app/components/Product.jsx
+++ src/app/components/Product.jsx
@@ -1,16 +1,34 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Product({ data, border }) {
   const spanStyles =
-    "col-span-2 justify-self-start px-4 py-2 bg-[#F2F4FF] text-[#4661E6] font-bold rounded-full";
+    "col-span-2 justify-self-start px-4 py-2 bg-[#F2F4FF] text-[#4661E6] font-bold rounded-xl md:col-span-1 md:col-start-2";
   return (
     <div className={border ? `border-t-8 rounded-md ${border}` : null}>
       <Link href={`/${data.productid}`} className="block">
-        <div className="bg-white p-5 rounded-md grid grid-cols-2 gap-2">
-          <p className="font-bold col-span-2">{data.title}</p>
-          <p className="text-gray-500 col-span-2">{data.description}</p>
+        <div className="bg-white p-5 rounded-md grid grid-cols-2 gap-2 md:grid-cols-7 md:gap-1">
+          <p className="font-bold col-span-2 md:col-start-2 md:col-span-7">
+            {data.title}
+          </p>
+          <p className="text-gray-500 col-span-2 md:col-start-2 md:col-span-7">
+            {data.description}
+          </p>
           <span className={spanStyles}>{data.category}</span>
-          <span className={spanStyles}>{data.upvotes}</span>
+          <span
+            className={
+              "col-span-2 justify-self-start px-4 py-2 bg-[#F2F4FF] text-[#4661E6] font-bold rounded-xl flex items-center gap-2 md:col-span-1 md:col-start-1 md:row-start-1"
+            }
+          >
+            <Image
+              src={"/icon-arrow-down.svg"}
+              width={11}
+              height={7}
+              alt="Upvotes arrow"
+              className="rotate-180"
+            />
+            {data.upvotes}
+          </span>
         </div>
       </Link>
     </div>
