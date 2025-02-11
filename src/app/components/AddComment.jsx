d8b67b683ebdc4ac632420e68ef0447c34c3dab0
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2025-02-11 11:59:18 -0500

    Updates
    Comment section is working!

--- src/app/components/AddComment.jsx
+++ src/app/components/AddComment.jsx
@@ -1,14 +1,24 @@
 import Button from "./Button";
-export default function AddComment() {
+import postComment from "../lib/postComment";
+
+export default async function AddComment({ id }) {
+  const postCommentWithID = postComment.bind(null, id);
   return (
     <div className="bg-white p-5 rounded-md space-y-5">
       <h1 className="text-2xl font-bold">Add Comment</h1>
-      <form>
+      <form action={postCommentWithID}>
+        <input className="hidden" name="full_name" defaultValue={"Jio Pacho"} />
+        <input
+          className="hidden"
+          name="username"
+          defaultValue={"jiobautista__"}
+        />
         <textarea
           cols={50}
           rows={3}
           className="w-full p-5 bg-[#F7F8FD]"
           placeholder="Type your comment here"
+          name="content"
         />
         <div className="flex items-center justify-between mt-5">
           <p>250 Characters left</p>
